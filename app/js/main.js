4cbbb5d5c314ad8f10b38abd75f2bc15e3c9748e
Melkorich <Kirill.Lavrov92@gmail.com>
2021-08-23 01:02:44 +0300

    header and about finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
+
+  $('.header__slider').slick({
+    arrows: false,
+    fade: true,
+    asNavFor: '.header__slider-dots',
+  });
+
+  $('.header__slider-dots').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    asNavFor: '.header__slider',
+  });
  
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,10 +27,11 @@ function styles(){
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
 }
-
+ 
 function scripts(){
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
