7d4ef110e03ed5235831489035b1219df19cc6c7
Melkorich <Kirill.Lavrov92@gmail.com>
2022-01-23 13:20:06 +0300

    slider fixes

--- app/js/main.js
+++ app/js/main.js
@@ -80,31 +80,28 @@ $(function(){
 
 new WOW().init();
 
+let swiper = new Swiper(".tours__slider", {
+  loop: true,
+  centeredSlides: true,
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }, 
 
-  // const slider = document.querySelector('.swiper-container');
-  let swiper = new Swiper(".tours__slider", {
-    loop: true,
-    centeredSlides: true,
-    
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }, 
-
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        // centeredSlides: false,
-      },
-
-      1040: {
-        slidesPerView: 'auto',
-        spaceBetween: 120,
-        centeredSlides: true,
-        grabCursor: true,
-      },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      centeredSlides: true,
     },
 
+    1040: {
+      slidesPerView: 'auto',
+      spaceBetween: 120,
+      centeredSlides: true,
+      grabCursor: true,
+    },
+  },
 
-  });
+});
